5378119e6b49c9d3a457a13c979b8410dd98cf0d
Szymon <strzelecszymon.3@gmail.com>
2023-03-27 13:43:59 +0200

    build the app

--- src/components/AsideMenu/Streaminfo.js
+++ src/components/AsideMenu/Streaminfo.js
@@ -2,7 +2,7 @@
 const Streaminfo = () => {
 
   return (
-    <div className={'fixed h-12 w-48 bg-black-700 rounded px-2 font-light text-sm animate-appear left-[240px]'}>
+    <div className={'fixed left-[245px] h-12 w-48 bg-black-700 rounded px-2 font-light text-sm animate-appear'}>
       <p className='h-full w-full line-clamp-2 p-1'>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis illum a non numquam nesciunt quia distinctio quod labore suscipit
       </p> 
